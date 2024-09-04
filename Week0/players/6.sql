1d9d8b2dd20d84c2a4eb01dfebf5c40bc3362240
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-04 07:59:02 +0300

    Add files via upload

---
+++ Week0/players/6.sql
@@ -0,0 +1,8 @@
+﻿SELECT COUNT(*)
+FROM players
+WHERE (bats = 'R' AND throws = 'L') OR
+(bats = 'L' AND throws = 'R');
+
+--Bu sorgu, sağ elle vurup sol elle atan (bats = 'R' AND throws = 'L') veya sol elle vurup sağ elle atan (bats = 'L' AND throws = 'R') oyuncuların sayısını verir.
+
+
---
+++ Week0/players/7.sql
@@ -0,0 +1,7 @@
+﻿SELECT AVG(height) as avg_height, AVG(weight) as avg_weight
+FROM players
+WHERE birth_year >= '1850';
+
+
+--Bu sorgu, 1850'den tarihinden sonra dogan oyuncuların ortalama boyunu ("height") ve kilosunu ("weight") verir.
+
