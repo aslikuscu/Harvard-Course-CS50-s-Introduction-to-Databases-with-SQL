62e249cf9758a53a9f0babc79f63b46e9c1f2d84
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-06 06:43:00 +0300

    Add files via upload

---
+++ week1/dese/7.sql
@@ -0,0 +1,7 @@
+﻿SELECT s.name
+FROM schools s
+JOIN districts d ON s.id = d.id
+WHERE d.name LIKE 'cambridge'
+
+
+--Bu sorgu, adı "cambridge" ile başlayan bölgelerdeki okulların isimlerini getirir.
