e264e715d75f976096df095232e80bbaa5ff48fd
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-07 10:09:30 +0300

    Add files via upload

---
+++ week1/dese/11.sql
@@ -0,0 +1,10 @@
+﻿SELECT s.name, e.per_pupil_expenditure, g.graduated
+FROM schools s
+JOIN expenditures e ON s.id = e.id
+JOIN graduation_rates g ON s.id = g.id
+ORDER BY e.per_pupil_expenditure DESC, s.name
+
+--3 tablo birlestirildi
+--Bu sorgu, okulların adlarını, öğrenci başına harcamayı ve mezuniyet oranlarını getirir. Sonuçları, harcamaya göre azalan sırada ve ardından okul adı sırasına göre sıralar.
+
+
