bd21b8ecf42c04c7ecf822fc8f4fd7415d476c3f
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-07 10:09:14 +0300

    Add files via upload

---
+++ week1/dese/10.sql
@@ -0,0 +1,8 @@
+﻿SELECT TOP 10 d.name, e.per_pupil_expenditure
+FROM expenditures e
+JOIN districts d ON e.id = d.id
+WHERE d.type = 'Public School District'
+ORDER BY e.per_pupil_expenditure DESC 
+
+--Bu sorgu, halka acik okul bölgeleri içindeki en yüksek öğrenci başına harcamaya sahip ilk 10 bölgenin adlarını ve harcama miktarlarını getirir.
+
