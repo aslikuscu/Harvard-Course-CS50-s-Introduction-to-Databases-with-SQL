183f1c81916246ca9ee51c5c03f54d6e2b39dc07
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-10 20:52:24 +0300

    Add files via upload

---
+++ week1/moneyball/2.sql
@@ -0,0 +1,9 @@
+﻿SELECT p.first_name, p.last_name, s.salary
+FROM players p
+JOIN salaries s ON p.id = s.id
+WHERE s.year = '1985'
+ORDER BY s.salary, p.first_name, p.last_name
+
+
+--Bu sorgu, 1985 yılındaki maaşlarla oyuncuların isimlerini getirir ve maaşa, ardından isimlere ve ID'ye göre sıralar.
+
