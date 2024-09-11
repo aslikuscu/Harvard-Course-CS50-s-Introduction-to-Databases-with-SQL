1e75404d0db1c74998c2af1f54239b203ac42bdd
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-11 23:18:50 +0300

    Add files via upload

---
+++ week1/moneyball/4.sql
@@ -0,0 +1,10 @@
+﻿SELECT TOP 5 t.name, SUM(p.H) AS total_hits
+FROM performances p
+JOIN teams t ON p.id = t.id
+WHERE p.year = '1871'
+GROUP BY t.name
+ORDER BY total_hits DESC
+
+--Bu sorgu, 1871 yılında en çok vuruş yapan ilk 5 takımın adlarını ve toplam vuruş sayılarını getirir.
+
+
