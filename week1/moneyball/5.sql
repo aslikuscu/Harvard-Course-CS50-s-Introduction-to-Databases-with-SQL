e0c160c5af8e48d2dbdfefb92e1b48453ef25a7c
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-12 16:42:18 +0300

    Add files via upload

---
+++ week1/moneyball/5.sql
@@ -0,0 +1,7 @@
+﻿SELECT pl.first_name, pl.last_name
+FROM players pl
+JOIN salaries s ON pl.id = s.id
+WHERE s.salary = (SELECT MAX(salary) FROM salaries)
+
+
+--Bu sorgu, en yüksek maaşı alan oyuncuların isimlerini getirir.
