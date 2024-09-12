c8a34f451cb6a6f5bcedc8b7305fe51b65ee82ea
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-12 16:42:43 +0300

    Add files via upload

---
+++ week1/moneyball/7.sql
@@ -0,0 +1,9 @@
+﻿SELECT pl.first_name, pl.last_name, s.salary, p.HR, p.year
+FROM performances p
+JOIN salaries s ON p.id = s.id AND p.year != s.year
+JOIN players pl ON p.id = pl.id
+ORDER BY pl.id, p.year DESC, p.HR DESC, s.salary DESC;
+
+
+--Bu sorgu, oyuncuların isimlerini, maaşlarını, home run sayılarını ve performans yıllarını getirir. Sonuçları oyuncu ID'sine, yıl bazında azalan sıraya, home run sayısına ve maaşa göre sıralar.
+
