8fe60fca49b0910917231c2131e7f5a852d9543f
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-13 23:37:45 +0300

    Add files via upload

---
+++ week1/packages/1.sql
@@ -0,0 +1,19 @@
+﻿SELECT s.address_id, s.action, s.timestamp
+FROM scans s
+INNER JOIN packages p ON s.package_id = p.id
+INNER JOIN addresses a ON p.from_address_id = a.id
+WHERE a.address = '900 Somerville Avenue'
+
+SELECT address_id, action, timestamp
+FROM scans
+WHERE package_id = (
+    SELECT id
+    FROM packages
+    WHERE from_address_id = (
+        SELECT id
+        FROM addresses
+        WHERE address = 900 Somerville Avenue)
+    )
+
+--Bu sorgu, "900 Somerville Avenue" adresinden gönderilen paketin tarama kayıtlarını getirir. Adresin ID'si ile paketin ID'sini bulur ve ilgili tarama kayıtlarını address_id, action, ve timestamp bilgileri ile döndürür.
+
