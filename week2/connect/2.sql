c03732b990d33861ab7c6698d019f822feca2af4
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-17 15:14:57 +0300

    Add files via upload

---
+++ week2/connect/2.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE schools_university(
+    id INT,
+    name NVARCHAR(255) NOT NULL,
+    type NVARCHAR(50) NOT NULL CHECK(type IN ('elementary school', 'middle school',
+        'high school', 'lower school', 'upper school', 'college', 'university')),
+    location NVARCHAR(255) NOT NULL,
+    date_founded INT,
+    PRIMARY KEY (id)
+)
+
+-- CHECK kısıtlaması, type sütununda sadece belirli değerlerin kullanılmasını sağlar. 
