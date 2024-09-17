199a44f0527ea96391f0ae151d65dc9fbd25851b
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-17 15:14:36 +0300

    Add files via upload

---
+++ week2/connect/1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE users (
+    id INT,
+    first_name NVARCHAR(100) NOT NULL,
+    last_name NVARCHAR(100) NOT NULL,
+    username NVARCHAR(255) NOT NULL UNIQUE,
+    password NVARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+--yeni tablo olusturuldu 
+--PRIMARY KEY, bir tabloya eklenen ve o tablo için benzersiz bir tanımlayıcı (ID) görevi gören bir kısıtlamadır. 
