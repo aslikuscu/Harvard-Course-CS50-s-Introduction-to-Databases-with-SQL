4e02740f34e20f49b55fb861dfdd72e339156fc2
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-20 00:20:08 +0300

    Add files via upload

---
+++ week3/dont-panic/5.sql
@@ -0,0 +1,8 @@
+﻿UPDATE temp
+SET mass = NULL,
+    year = NULL,
+    lat = NULL,
+    long = NULL
+WHERE mass = '' OR year = '' OR lat = '' OR long = ''
+
+--Bu komut, temp tablosundaki boş (boşluk karakteri içeren) değerleri NULL olarak günceller.
