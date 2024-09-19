7f232a9aa22b99752c727c172c27781e65105b1d
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-20 00:17:25 +0300

    Add files via upload

---
+++ week3/dont-panic/1.sql
@@ -0,0 +1,10 @@
+﻿UPDATE users
+SET password = '982c0381c279d139fd221fce974916e7'
+WHERE username = 'admin'
+
+--UPDATE komutu, veritabanındaki mevcut bir tablodaki verileri güncellemek için kullanılır.
+
+--SET ifadesi, güncellenmesi gereken sütunları ve bu sütunlara atanacak yeni değerleri belirtir.
+
+--username = 'admin': Bu koşul, yalnızca username sütununda 'admin' değeri bulunan satırların güncellenmesini sağlar. Yani, sadece kullanıcı adı 'admin' olan kullanıcının şifresi değiştirilecektir.
+
