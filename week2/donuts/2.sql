53b7017dc592044b9207a0e4e64a7638787e0c41
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-19 10:07:34 +0300

    Add files via upload

---
+++ week2/donuts/2.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE donuts(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    gluten_free INTEGER NOT NULL CHECK (gluten_free IN ('1', '0')),
+    sell_price INTEGER NOT NULL,
+    ingredients TEXT NOT NULL
+)
+
+--gluten_free INTEGER NOT NULL CHECK (gluten_free IN ('1', '0')) ifadesi, gluten_free sütununun sadece iki değeri alabileceğini belirten bir kısıtlamadır: 1 (glutensiz) veya 0 (gluten içerir). Bu şekilde, sütuna başka bir değer girilmesi engellenir.
+
