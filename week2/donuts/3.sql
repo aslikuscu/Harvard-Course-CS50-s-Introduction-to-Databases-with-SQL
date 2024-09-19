14396e60da81f129d0db593fce84a7d3bd4be88a
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-19 10:07:45 +0300

    Add files via upload

---
+++ week2/donuts/3.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE orders(
+    id INTEGER PRIMARY KEY,
+    donuts TEXT,
+    customer_id INTEGER,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+)
+--PRIMARY KEY (birincil anahtar), bir tablodaki her bir kaydı benzersiz bir şekilde tanımlamak için kullanılan bir kısıtlamadır.
