5aa7d04d01fdd72103451f692f48e0f2a5c9a68b
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-20 00:20:53 +0300

    Add files via upload

---
+++ week3/dont-panic/9.sql
@@ -0,0 +1,9 @@
+﻿INSERT INTO meteorites (name, class, mass, discovery, year, lat, long)
+SELECT name, class, mass, discovery, year, lat, long
+FROM temp
+ORDER BY year, name
+
+--temp tablosunda bulunan veriler, meteorites tablosuna aktarılıyor.
+
+--Veriler önce year sütununa göre, aynı yıl içerisindeki veriler ise name sütununa göre alfabetik olarak sıralanıyor.
+
