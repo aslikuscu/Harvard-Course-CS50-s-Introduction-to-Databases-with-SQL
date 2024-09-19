74f4ee9b5d83f42f9bcd6395ffeed1889fb1669f
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-20 00:17:47 +0300

    Add files via upload

---
+++ week3/dont-panic/3.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO "user_logs" ("type", "old_username", "new_username", "old_password", "new_password")
+VALUES ("update", "admin", "admin", "e10adc3949ba59abbe56e057f20f883e", "44bf025d27eea66336e5c1133c3827f7");
+
+
+--Bu komut, user_logs tablosuna yeni bir güncelleme kaydı ekler. Bu kayıtta admin kullanıcısının eski ve yeni şifreleri ile diğer ilgili bilgiler bulunur.
+
