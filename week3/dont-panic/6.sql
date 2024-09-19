c66e616185ff548bc3cab06d06b33674a1c8075e
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-20 00:20:17 +0300

    Add files via upload

---
+++ week3/dont-panic/6.sql
@@ -0,0 +1,7 @@
+﻿UPDATE temp
+SET mass = ROUND(mass, 2),
+    lat = ROUND(lat, 2),
+    long = ROUND(long, 2)
+
+--Bu komut, temp tablosundaki mass, lat ve long sütunlarındaki değerleri iki ondalık basamağa yuvarlar.
+
