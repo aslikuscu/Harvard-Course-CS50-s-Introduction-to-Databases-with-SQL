f8410b3de0394807a216b7f24435ed3ca9efad1c
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-20 00:20:38 +0300

    Add files via upload

---
+++ week3/dont-panic/8.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE meteorites (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255),
+    class VARCHAR(255),
+    mass REAL,
+    discovery VARCHAR(10) CHECK (discovery IN ('Fell', 'Found')),
+    year INTEGER,
+    lat REAL,
+    long REAL
+)
+
+--real kismi  ondalıklı sayıları saklamak için kullanılır.
+
+--CHECK kısıtlaması, sütunun sadece belirli değerler almasını sağlar. 
