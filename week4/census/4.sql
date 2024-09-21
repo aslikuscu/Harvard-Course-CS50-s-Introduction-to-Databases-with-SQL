f9e9f87cc44b5774207c8cba9da255f26f51256d
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-21 10:09:57 +0300

    Add files via upload

---
+++ week4/census/4.sql
@@ -0,0 +1,12 @@
+﻿CREATE VIEW total AS
+SELECT 
+    SUM(families) AS total_families, 
+    SUM(households) AS total_households, 
+    SUM(population) AS total_population, 
+    SUM(male) AS total_male, 
+    SUM(female) AS total_female
+FROM census
+
+
+--Bu komut, census tablosundaki tüm verilerin toplamını hesaplar. Toplam aile sayısı, hanehalkı sayısı, nüfus, erkek ve kadın sayıları hesaplanır ve total adlı bir görünümde saklanır.
+
