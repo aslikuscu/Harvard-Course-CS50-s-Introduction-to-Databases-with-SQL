107d0a1f5823f6cabc427aff970ad4b9a5b07f06
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-23 16:52:07 +0300

    Add files via upload

---
+++ week4/private/3.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW message AS
+SELECT SUBSTRING(s.sentence, t.second, t.third) AS phrase
+FROM sentences s
+JOIN text t ON s.id = t.first
+WHERE t.first != ''
+
+--substr(s."sentence", t."second", t."third"): Bu fonksiyon, sentences tablosundaki sentence sütunundan bir alt dize (substring) alır. Bu alt dize, text tablosundaki second ve third sütunları tarafından belirtilen konum ve uzunlukta alınır.
+
+--WHERE ifadesi, text tablosundaki first sütununda boş olmayan satırları filtreler.
+
