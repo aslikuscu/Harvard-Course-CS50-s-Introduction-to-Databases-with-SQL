62baa76536d26c9e8301236c3bb40f21937770af
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-23 16:51:56 +0300

    Add files via upload

---
+++ week4/private/2.sql
@@ -0,0 +1,13 @@
+﻿INSERT INTO text(first, second, third)
+    VALUES
+    (14,98,4),
+    (114,3,5),
+    (618,72,9),
+    (630,7,3),
+    (932,12,5),
+    (2230,50,7),
+    (2346,44,10),
+    (3041,14,5)
+
+--Bu komut, text tablosuna birkaç satır veri ekler.
+
