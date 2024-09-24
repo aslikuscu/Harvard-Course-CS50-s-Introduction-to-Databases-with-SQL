8f469a53ed5217eabcb4baf0cd6723fe7b31f07f
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-24 10:30:44 +0300

    Add files via upload

---
+++ week5/harvard/1.sql
@@ -0,0 +1,12 @@
+﻿CREATE INDEX semester_2023_2024
+ON courses(semester)
+WHERE semester IN ('Fall 2024', 'Spring 2024', 'Fall 2023');
+
+
+--ON "courses" ifadesi, courses tablosu üzerinde bir indeks oluşturulacağını belirtir.
+
+--("semester") ifadesi, semester sütunu üzerinde bir indeks oluşturulacağını belirtir.
+
+--where kismi Bu ifade, indeksi yalnızca semester sütunundaki belirli değerler için oluşturur.
+
+--İndeksler, belirli sütunlarda arama işlemlerini hızlandırarak veritabanı sorgularının daha hızlı çalışmasını sağlar.
