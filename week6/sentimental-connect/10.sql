bebebe007a22e0fd7c1e2809a9cf9006960865ef
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-26 19:45:09 +0300

    Add files via upload

---
+++ week6/sentimental-connect/10.sql
@@ -0,0 +1,11 @@
+INSERT INTO "items" ("id", "name", "price")
+VALUES ('1', 'CS50 Rubber Duck', '4.99'),
+('2', 'CS50 Pen', '4.99'),
+('3', 'CS50 Sunglasses', '9.99'),
+('4', 'CS50 Unisex T-Shirt', '30.00'),
+('5', 'CS50 Hoodie', '42.00'),
+('6', 'CS50 Toddler Shirt', '20.00'),
+('7', 'CS50 Pillow', '28.00'),
+('8', 'CS50 Mug', '14.00'),
+('9', 'CS50 Embroidered Beanie', '24.00'),
+('10', 'CS50 Kids T-Shirt', '25.00');
