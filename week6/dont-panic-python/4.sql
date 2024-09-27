cae9fa12471341ac8227496acfffa379c96ad1ec
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-27 16:07:50 +0300

    Add files via upload

---
+++ week6/dont-panic-python/4.sql
@@ -0,0 +1,14 @@
+﻿
+
+
+CREATE TABLE `PeopleConnections` (
+    `id` INT AUTO_INCREMENT,
+    `user_id` INT,
+    `following_id` INT,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `Users`(`id`),
+    FOREIGN KEY (`following_id`) REFERENCES `Users`(`id`),
+    CONSTRAINT `unique_connection` UNIQUE (`user_id`, `following_id`)
+)
+
+----Bu CONSTRAINT (yani kısıtlama), user_id ve following_id sütunlarının birleşiminin her satırda benzersiz olmasını sağlar.
