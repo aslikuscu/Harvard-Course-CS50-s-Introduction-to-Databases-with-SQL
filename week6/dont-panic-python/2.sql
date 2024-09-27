07795b5aa7dee738a825fb2874fb593d560c8caa
Aslı Kuşçu <115110503+aslikuscu@users.noreply.github.com>
2024-09-27 16:07:30 +0300

    Add files via upload

---
+++ week6/dont-panic-python/2.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE `Schools` (
+    `id` INT AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `type` ENUM('Primary', 'Secondary', 'Higher Education'),
+    `location` VARCHAR(255),
+    `year_founded` INT,
+    PRIMARY KEY (`id`)
+)
+
+--ENUM veri tipi, bir sütunun belirli bir dizi değer arasından seçim yapmasını sağlamak için kullanılır. Yani, sütunun alabileceği değerler önceden tanımlanır ve sadece bu değerler kabul edilir. ENUM, belirli ve sınırlı bir seçenek kümesi olduğunda kullanılır.
+
